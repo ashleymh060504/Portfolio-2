6d603d5b39876bc08e2c85227c4f165e997a153a
Ashley Hayes <ashleymh060504@gmail.com>
2025-02-07 13:50:07 -0500

    Added react router to dependencies and App.jsx, edited Header, added pages dir and 4 pgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      {/* Other sections */}
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,14 +6,15 @@ const Header = () => {
     <Navbar expand="lg" style={{ backgroundColor: 'FFCDB2' }} variant="dark">
       <Container>
         <Navbar.Brand href="#" style={{ color: '#6D6875', fontWeight: 'bold' }}>
-          MyPortfolio
+          Ashley Hayes
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#about" style={{ color: '#6D6875' }}>About</Nav.Link>
-            <Nav.Link href="#projects" style={{ color: '#6D6875' }}>Projects</Nav.Link>
+            <Nav.Link href="#about" style={{ color: '#6D6875' }}>About Me</Nav.Link>
+            <Nav.Link href="#projects" style={{ color: '#6D6875' }}>Portfolio</Nav.Link>
             <Nav.Link href="#contact" style={{ color: '#6D6875' }}>Contact</Nav.Link>
+            <Nav.Link href="#contact" style={{ color: '#6D6875' }}>Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
