7ac1a690aabc6ad870828ec24af57ecf8ddd14f0
Ashley Hayes <ashleymh060504@gmail.com>
2025-02-11 11:19:28 -0500

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer style={{ backgroundColor: '#FFCDB2', color: '#6D6875', padding: '1rem', marginTop: '5rem' }}>
       <Container className="text-center">
         <p className="mb-0">
-          &copy; {new Date().getFullYear()} Ashley Hayes | Built with ❤️ using React & Bootstrap
+          &copy; {new Date().getFullYear()} Ashley Hayes <a href="https://github.com/ashleymh060504">Github</a> <a href="https://www.linkedin.com/in/ashley-hayes-70654a261/">LinkedIn</a>
         </p>
       </Container>
     </footer>
